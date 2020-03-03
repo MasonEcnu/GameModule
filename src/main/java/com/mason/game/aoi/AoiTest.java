042d8d66370a9b49ffa2342adbede7502be0042d
MasonEcnu <364207187@qq.com>
2020-03-03 19:36:20 +0800

    调试Java版的AOI视野管理器1

--- src/main/java/com/mason/game/aoi/AoiTest.java
+++ src/main/java/com/mason/game/aoi/AoiTest.java
@@ -21,7 +21,6 @@ public class AoiTest {
     for (int i = 1; i <= 20000; i++) {
       int posX = Tools.random(1, MAP_MAX_WITH);
       int posY = Tools.random(1, MAP_MAX_HEIGHT);
-
       String cId = Tools.generateCreateName(i);
       Creature creature = new Creature(cId, posX, posY);
       creatures.put(cId, creature);
@@ -34,20 +33,10 @@ public class AoiTest {
     long start = System.currentTimeMillis();
     AtomicInteger index = new AtomicInteger();
     creatures.forEach((cid, creature) -> {
-      int oldPosX = creature.getPosX();
-      int oldPosY = creature.getPosY();
-      int posX, posY;
-      while (true) {
-        posX = oldPosX + Tools.random(-5, 5);
-        posY = oldPosY + Tools.random(-5, 5);
-        if (mapGrids.isPosValid(posX, posY)) {
-          creature.setPosX(posX);
-          creature.setPosY(posY);
-          break;
-        }
-      }
-      System.out.println(index.getAndIncrement());
-      mapGrids.move(oldPosX, oldPosY, posX, posY, cid);
+      move(mapGrids, creature);
+      index.getAndIncrement();
+      System.out.println("start move:" + index.get());
+      System.out.println("end move:" + index.get());
     });
     long end = System.currentTimeMillis();
     System.out.format("All creatures move one time used %s seconds", (end - start) / SECOND_TO_MILLIS);
@@ -57,22 +46,26 @@ public class AoiTest {
     int ranCid = Tools.random(1, 20000);
     Creature creature = creatures.get(Tools.generateCreateName(ranCid));
     for (int i = 1; i <= 20000; i++) {
-      int oldPosX = creature.getPosX();
-      int oldPosY = creature.getPosY();
-      int posX, posY;
-      while (true) {
-        posX = oldPosX + Tools.random(-10, 10);
-        posY = oldPosY + Tools.random(-10, 10);
-        if (mapGrids.isPosValid(posX, posY)) {
-          creature.setPosX(posX);
-          creature.setPosY(posY);
-          break;
-        }
-      }
-      MoveResult moveResult = mapGrids.move(oldPosX, oldPosY, posX, posY, creature.getId());
+      MoveResult moveResult = move(mapGrids, creature);
       System.out.println(moveResult);
     }
     end = System.currentTimeMillis();
     System.out.format("One creature move 20000 time used %s seconds", (end - start) / SECOND_TO_MILLIS);
+  }
+
+  private static MoveResult move(GridManager mapGrids, Creature creature) {
+    int oldPosX = creature.getPosX();
+    int oldPosY = creature.getPosY();
+    int posX, posY;
+    while (true) {
+      posX = oldPosX + Tools.random(-10, 10);
+      posY = oldPosY + Tools.random(-10, 10);
+      if (mapGrids.isPosValid(posX, posY)) {
+        creature.setPosX(posX);
+        creature.setPosY(posY);
+        break;
+      }
+    }
+    return mapGrids.move(oldPosX, oldPosY, posX, posY, creature.getId());
   }
 }
